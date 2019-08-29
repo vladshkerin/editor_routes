f26624e9d45d02337d3d67c46c240eb554ac2046
Vladimir Shkerin <vladshkerin@gmail.com>
2019-08-29 17:23:34 +0300

    Fix spaces

--- src/containers/MapContainer.js
+++ src/containers/MapContainer.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 class MapContainer extends Component {
-    render() {
-        return (
-            <section className="map-wrapper">
-                <div id="map"/>
-            </section>
-        );
-    }
+  render() {
+    return (
+      <section className="map-wrapper">
+        <div id="map"/>
+      </section>
+    )
+  }
 }
 
 export default MapContainer
--- src/containers/PointContainer.js
+++ src/containers/PointContainer.js
@@ -1,21 +1,21 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class PointContainer extends Component {
-    render() {
-        const {number} = this.props;
+  render() {
+    const { number } = this.props
 
-        return (
-            <div className="point">
-                <label htmlFor="point__name">Точка маршрута {number}</label>
-                <input id="point__name" type="checkbox" name="name"/>
-            </div>
-        )
-    }
+    return (
+      <div className="point">
+        <p className={'point__name-' + number}>Точка маршрута {number}</p>
+        <button className={'point__btn-' + number}>x</button>
+      </div>
+    )
+  }
 }
 
 PointContainer.propTypes = {
-    number: PropTypes.number,
-};
+  number: PropTypes.number,
+}
 
 export default PointContainer
