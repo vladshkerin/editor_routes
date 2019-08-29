fe1125df77bed588bcd988975128945f9eb667b7
Vladimir Shkerin <vladshkerin@gmail.com>
2019-08-29 15:58:42 +0300

    Creat container "MapContainer.js"

---
+++ src/containers/MapContainer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class MapContainer extends Component {
+    render() {
+        return (
+            <section className="map-wrapper">
+                <div id="map"/>
+            </section>
+        );
+    }
+}
+
+export default MapContainer
